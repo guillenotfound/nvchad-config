35d2e0ce3ea3d03b4df98ea65371e4d4e7a30dd3
Guille <guillenotfound@gmail.com>
2023-03-19 01:17:04 +0100

    initial nvchad config

--- mappings.lua
+++ mappings.lua
@@ -2,8 +2,52 @@
 local M = {}
 
 M.general = {
+  i = {
+    ["<C-d>"] = { "<Del>", "forward delete" },
+
+    ["<esc>"] = { "<Nop>", "disable Esc to exit insert mode" },
+  },
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    ["<C-d>"] = { "<C-d>zz" },
+    ["<C-u>"] = { "<C-u>zz" },
+
+    ["n"] = { "nzzzv" },
+    ["N"] = { "Nzzzv" },
+
+    ["<leader>|"] = { "<C-w>v" },
+    ["<leader>-"] = { "<C-w>s" },
+
+    -- disable direction keys
+    ["<up>"] = { "<Nop>" },
+    ["<down>"] = { "<Nop>" },
+    ["<left>"] = { "<Nop>" },
+    ["<right>"] = { "<Nop>" },
+  },
+  v = {
+    ["K"] = { ":m '<-2<CR>gv=gv" },
+    ["J"] = { ":m '>+1<CR>gv=gv" },
+    ["<A-j>"] = { ":m .+1<CR>==" },
+    ["<A-k>"] = { ":m .-2<CR>==" },
+  },
+  x = {
+    ["J"] = { ":move '>+1<CR>gv-gv" },
+    ["K"] = { ":move '<-2<CR>gv-gv" },
+    ["<A-j>"] = { ":move '>+1<CR>gv-gv" },
+    ["<A-k>"] = { ":move '<-2<CR>gv-gv" },
+  },
+}
+
+M.telescope = {
+  plugin = true,
+
+  n = {
+    -- find
+    ["<leader>sf"] = { "<cmd> Telescope find_files <CR>", "find files" },
+    ["<leader>sg"] = { "<cmd> Telescope live_grep <CR>", "live grep" },
+    ["<leader><leader>"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
+    ["<leader>?"] = { "<cmd> Telescope help_tags <CR>", "help page" },
   },
 }
 
--- plugins.lua
+++ plugins.lua
@@ -1,4 +1,4 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
@@ -25,7 +25,7 @@ local plugins = {
   -- override plugin configs
   {
     "williamboman/mason.nvim",
-    opts = overrides.mason
+    opts = overrides.mason,
   },
 
   {
@@ -36,6 +36,9 @@ local plugins = {
   {
     "nvim-tree/nvim-tree.lua",
     opts = overrides.nvimtree,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
   },
 
   -- Install a plugin
@@ -46,6 +49,20 @@ local plugins = {
       require("better_escape").setup()
     end,
   },
+
+  -- Git related
+  { "tpope/vim-fugitive" },
+
+  { "tpope/vim-rhubarb" },
+
+  {
+    "tommcdo/vim-fubitive",
+    config = function()
+      vim.g.fubitive_domain_pattern = "bitbucket.corp.zscaler.com"
+    end,
+  },
+
+  { "tpope/vim-sleuth" },
 
   -- To make a plugin not be loaded
   -- {
